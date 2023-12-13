a6b072314a701aa6c3ffe5d4fc24fab23847b883
djwjfk <grw07236@naver.com>
2023-12-14 01:44:23 +0900

    art: 날짜 헤더 부분 커스텀 훅 사용

---
+++ src/hooks/useCurrentDate.tsx
@@ -0,0 +1,20 @@
+// useCurrentDate.tsx
+import { useState } from 'react';
+import { subMonths, addMonths } from 'date-fns';
+
+const useCurrentDate = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  //현재 달에서 -1
+  const handlePrevMonth = () => {
+    setCurrentDate((prevDate) => subMonths(prevDate, 1));
+  };
+  //현재 달에서 +1
+  const handleNextMonth = () => {
+    setCurrentDate((prevDate) => addMonths(prevDate, 1));
+  };
+
+  return { currentDate, handlePrevMonth, handleNextMonth };
+};
+
+export default useCurrentDate;
