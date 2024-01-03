35ae81903c6ec33af3999b4039eb874b272104ad
djwjfk <grw07236@naver.com>
2024-01-03 16:40:13 +0900

    art: 검색기능 모달컴포넌트로 관리

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import _debounce from 'lodash/debounce';
 import instanceAxios from '../api/InstanceAxios';
+import SchoolSearchModal from '../components/SchoolSearchModal';
 
 interface InfoDate {
   name: string;
@@ -13,23 +14,31 @@ interface InfoDate {
 
 const Join = (props: { name: string }) => {
   const navigate = useNavigate();
-  const [selectedGubun, setSelectedGubun] = useState(''); // 초등학교, 중학교, 고등학교 중 선택된 값
+  //**상태관리**//
   const [infoFormData, setInfoFormData] = useState<InfoDate>({
     name: '',
     subject: '',
     year: '',
     school: '',
   });
-  const [searchResults, setSearchResults] = useState<any[]>([]); //검색결과를 저장할 상태
+  const [schoolSearchClick, setSchoolSearchClick] = useState<Boolean>(false); //학교 검색 모달 열기용
 
-  const apiKey: string | undefined = process.env.REACT_APP_API_KEY;
-  const apiUrl: string | undefined =
-    'http://www.career.go.kr/cnet/openapi/getOpenApi.json';
+  //**이벤트 핸들러**//
 
   const handleCancelBtn = () => {
     navigate(-1);
   };
 
+  //학교 검색 모달 열기 함수
+  const handleSchoolSearchClick = () => {
+    setSchoolSearchClick(true);
+  };
+  // 학교 검색 모달 닫기 함수
+  const handleCloseModal = () => {
+    setSchoolSearchClick(false);
+  };
+
+  // 서버에 보내줄 값 함수
   const handleInfoConfirmBtn = () => {
     if (
       infoFormData.name &&
@@ -63,56 +72,8 @@ const Join = (props: { name: string }) => {
       //   alert('내용을 모두 입력해주세요');
     }
   };
-  //학교정보 받아오기
-  const handleSchoolSearch = async () => {
-    if (infoFormData.school && selectedGubun) {
-      try {
-        const response = await axios.get(apiUrl, {
-          params: {
-            apiKey: apiKey,
-            svcType: 'api',
-            svcCode: 'SCHOOL',
-            gubun: selectedGubun,
-            searchSchulNm: infoFormData.school,
-          },
-        });
 
-        //응답들어오는 XML 형식에서 특정 데이터 추출하기
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(response.data, 'application/xml');
-
-        //검색결과를 처리하는 함수 추가
-        const schools = Array.from(xmlDoc.querySelectorAll('content')).map(
-          (contentNode) => {
-            return {
-              seq: contentNode.querySelector('seq')?.textContent || '',
-              schoolName:
-                contentNode.querySelector('schoolName')?.textContent || '',
-              adres: contentNode.querySelector('adres')?.textContent || '',
-            };
-          },
-        );
-
-        setSearchResults(schools);
-      } catch (error) {
-        console.error('Error searching school information:', error);
-      }
-    }
-  };
-  const debouncedSearch = _debounce(handleSchoolSearch, 300);
-
-  useEffect(() => {
-    debouncedSearch();
-    return () => {
-      debouncedSearch.cancel();
-    };
-  }, [infoFormData.school, selectedGubun]);
-
-  const handleGubunButtonClick = (selectedValue: string) => {
-    // 사용자가 버튼을 클릭할 때 해당 버튼의 값을 설정
-    setSelectedGubun(selectedValue);
-  };
-
+  // 유저의 입력값이 변할때 발생하는 함수 (InfoFormData에 해당값 업데이트)
   const handleInfoChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ): void => {
@@ -170,51 +131,11 @@ const Join = (props: { name: string }) => {
       </div>
       <div>
         {' '}
-        <p>학교(검색)</p>
-        <input
-          type="text"
-          name="school"
-          value={infoFormData.school}
-          placeholder="학교를 입력해주세요."
-          onChange={handleInfoChange}
-        ></input>
-        <button type="button" onClick={handleSchoolSearch}>
-          학교 검색
-        </button>
-        <div>
-          <button
-            type="button"
-            onClick={() => handleGubunButtonClick('elem_list')}
-          >
-            초등학교
-          </button>
-
-          <button
-            type="button"
-            onClick={() => handleGubunButtonClick('midd_list')}
-          >
-            중학교
-          </button>
-
-          <button
-            type="button"
-            onClick={() => handleGubunButtonClick('high_list')}
-          >
-            고등학교
-          </button>
-        </div>
-      </div>
-      {/* 검색 결과 표시 */}
-      <div>
-        <h2>검색 결과</h2>
-        <ul>
-          {searchResults.map((result) => (
-            <li key={result.seq}>
-              <span>{result.schoolName}</span>
-              <p>{result.adres}</p>
-            </li>
-          ))}
-        </ul>
+        <p>학교</p>
+        <button onClick={handleSchoolSearchClick}>학교를 입력해주세요</button>
+        {schoolSearchClick ? (
+          <SchoolSearchModal onCloseModal={handleCloseModal} />
+        ) : null}
       </div>
       <div>
         <button type="submit" onClick={handleInfoConfirmBtn}>
