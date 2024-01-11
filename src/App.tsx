a3b893f8e6fc36f6ab93ed0df1d1d02c1e9f7222
djwjfk <grw07236@naver.com>
2024-01-12 01:34:42 +0900

    remove: 코드 전면 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import LeftSidebar from './components/LeftSidebar';
-import MainLayout from './components/MainLayout';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
-        {/* <Route element={<MainLayout />}> */}
-        <Route path="/home" element={<Home />} />
-        {/* </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div>APP</div>
+    </>
   );
 }
 
