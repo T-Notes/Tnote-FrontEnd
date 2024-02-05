af5ad62db5d83a1045cd262bc06e249d0e05a545
djwjfk <grw07236@naver.com>
2024-02-05 20:13:45 +0900

    Refactor : currentDate 값useHooks에서 받아오기

--- src/components/Home/RemainingDays.tsx
+++ src/components/Home/RemainingDays.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import instanceAxios from '../../utils/InstanceAxios';
 import { getRemainingDayData } from '../../utils/lib/api';
+import { useCurrentDate } from '../../utils/useHooks/useCurrentDate';
 
 const RemainingDays = () => {
   const { id } = useParams(); //현재 url에서 추출한 동적으로 변하는 id값
   console.log('남은학기 일수 scheduleId:', id);
   const [remainingDay, setRemainingDay] = useState<string | null>(null);
   // 고민1. formattedDate형식으로 넘겨주면 되는건가?
-  const currentDate = new Date();
+  const { currentDate } = useCurrentDate();
   const formattedDate = currentDate.toISOString().split('T')[0];
   // console.log('remainingDate:', formattedDate);
 
