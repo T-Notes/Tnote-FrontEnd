651767b11874788c2d22198b63261d991064ea27
djwjfk <grw07236@naver.com>
2024-02-21 19:25:15 +0900

    Style : 주석 삭제

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -10,24 +10,25 @@ import SemesterForm from './SemesterConfiguration/SemesterForm';
 import SemesterLayout from './SemesterLayout';
 import SemesterSetup from '../pages/SemesterSetup';
 import Callback from '../utils/Callback';
+import HomeNavigationBar from './common/HomeNavigationBar';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* left sidebar 없음 */}
         <Route path="/" element={<Landing />} />
+        <Route path="/profileInfo" element={<ProfileInfo />} />
+        <Route path="/login/oauth2/code/kakao" element={<Callback />} />
         <Route element={<HomeLayout />}>
           <Route path="/home/:id" element={<Home />} />
           {/* <Route element={<SemesterLayout />}> */}
-          {/* <Route path="/addSemester" element={<AddSemesterSetup />} /> */}
           <Route path="/semesterSetup/:id" element={<SemesterSetup />} />
           {/* </Route> */}
-
           <Route path="/timetable/:id" element={<Timetable />} />
         </Route>
-        {/* left sidebar 없음 */}
-        <Route path="/profileInfo" element={<ProfileInfo />} />
-        <Route path="/login/oauth2/code/kakao" element={<Callback />} />
+
+        {/* http://j9972.kr/login/oauth2/code/kakao  리다이렉트 주소 */}
       </Routes>
     </BrowserRouter>
   );
