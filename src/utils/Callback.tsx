71c8c09c9f4586a874492da2f4a26274c3002d71
djwjfk <grw07236@naver.com>
2024-02-22 22:51:55 +0900

    fix: 소셜로그인 1차 수정

--- src/utils/Callback.tsx
+++ src/utils/Callback.tsx
@@ -1,26 +1,34 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import instanceAxios from './InstanceAxios';
 import { kakaoLogin } from './lib/api';
 
 const Callback = () => {
   console.log(2, '실행!');
   const navigate = useNavigate();
+  const [code, setCode] = useState<string | null>(null);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
 
+  // 특정 키에 해당하는 쿼리 파라미터 값 가져오기
+  const myParamValue = queryParams.get('code');
+  // const code = new URL(window.location.href).searchParams.get('code'); // 인가코드 받아보기
+
+  // 서버주소로 돌아옴
   useEffect(() => {
+    console.log(3, '실행!');
     const getTokens = async () => {
       try {
-        const accessToken = localStorage.getItem('accessToken');
-        const refreshToken = localStorage.getItem('refreshToken');
-
-        if (!accessToken || !refreshToken) {
-          // 토큰이 없을 때 카카오 로그인을 호출합니다.
-          await kakaoLogin(); // kakaoLogin API를 호출하여 토큰을 받아옵니다.
-          // 받아온 토큰을 가지고 원하는 페이지로 리다이렉트합니다.
-          navigate('/profileInfo');
-        } else {
-          // 토큰이 이미 있으면 원하는 페이지로 리다이렉트합니다.
-          navigate('/home/:id');
-        }
+        await instanceAxios
+          .get(
+            `http://j9972.kr/login/oauth2/code/kakao?code=${code}&state=2vmvEBbhSq4ujp1WQbL1eh3VwSSGX6zck1AFq4XAXro%3D`,
+          )
+          .then((res: any) => {
+            console.log('res:', res);
+            localStorage.setItem('accessToken', res.data.accessToken);
+            localStorage.setItem('refreshToken', res.data.refreshToken);
+            navigate('/profileInfo');
+          });
       } catch (error) {
         console.error('토큰 가져오기 실패:', error);
       }
