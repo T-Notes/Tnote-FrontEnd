3e92a4d49bf3f7e198c867a9ea1bc15f9f29a850
djwjfk <grw07236@naver.com>
2024-03-08 02:46:42 +0900

    fix: 함수명 변경

--- src/components/SemesterConfiguration/LastClassList.tsx
+++ src/components/SemesterConfiguration/LastClassList.tsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-interface LastClassListProps {
-  onSelectedPeriod: (per: string) => void;
-}
 const SDropdownWrapper = styled.div`
   position: absolute;
   width: 100%;
@@ -27,24 +24,28 @@ const SItem = styled.li`
     background-color: #e6f6fc;
   }
 `;
-const LastClassList = ({ onSelectedPeriod }: LastClassListProps) => {
+
+interface LastClassListProps {
+  onSelectedSession: (per: string) => void;
+}
+const LastClassList = ({ onSelectedSession }: LastClassListProps) => {
   const lastClassList = [
-    { id: 1, period: '1교시' },
-    { id: 2, period: '2교시' },
-    { id: 3, period: '3교시' },
-    { id: 4, period: '4교시' },
-    { id: 5, period: '5교시' },
-    { id: 6, period: '6교시' },
-    { id: 7, period: '7교시' },
-    { id: 8, period: '8교시' },
-    { id: 9, period: '9교시' },
+    { id: 1, session: '1교시' },
+    { id: 2, session: '2교시' },
+    { id: 3, session: '3교시' },
+    { id: 4, session: '4교시' },
+    { id: 5, session: '5교시' },
+    { id: 6, session: '6교시' },
+    { id: 7, session: '7교시' },
+    { id: 8, session: '8교시' },
+    { id: 9, session: '9교시' },
   ];
   return (
     <SDropdownWrapper>
-      {lastClassList.map((per) => (
-        <SList key={per.id}>
-          <SItem onClick={() => onSelectedPeriod(per.period)}>
-            {per.period}
+      {lastClassList.map((session) => (
+        <SList key={session.id}>
+          <SItem onClick={() => onSelectedSession(session.session)}>
+            {session.session}
           </SItem>
         </SList>
       ))}
