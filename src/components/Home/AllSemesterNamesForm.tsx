9ad3d47f728a2ce52481c25dff98c34821c3c49e
djwjfk <grw07236@naver.com>
2024-03-09 00:47:46 +0900

    fix: 학기 전체 조회 드롭다운 리스트 공통컴포넌트화

--- src/components/Home/AllSemesterNamesForm.tsx
+++ src/components/Home/AllSemesterNamesForm.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Dropdown } from '../common/Dropdown';
 import { getAllSemesterNames, getSemesterData } from '../../utils/lib/api';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import DropdownInput from '../common/DropdownInput';
 import SemesterDropdownList from './SemesterDropdownList';
 import { useRecoilState, useRecoilValue } from 'recoil';
@@ -12,29 +12,6 @@ interface Semester {
   id: string;
   name: string;
 }
-// 정상적인 흐름
-// 유저가 처음 진입
-// 모든 서비스를 조회 가능
-// 새로운 학기를 추가하면
-// 해당 학기가 폼에 뜨고,
-// 해당학기를 바탕으로 서비스를 이용할 수 있어야 한다.
-
-// 지금 흐름
-// 유저가 처음 진입
-// 모든 서비스를 조회 가능
-// 새로운 학기를 추가하면 학기를 클릭하기 전까지 해당 학기를 가지고 서비스를 이용할 수 없음
-// 학기를 삭제해도 해당 학기가 params에 달려있어서 조회에러가 나면서 서비스가 죽어버림
-
-// 기존 회원 유저인 경우
-// 등록해 놓은 학기가 가장먼저 폼에 떠야한다.
-// 학기 아이디가 Params에 담겨야한다
-
-// 새 회원인 경우
-// 모든 서비스를 처음 이용하도록 초기화 상태여야 한다.
-
-//해결책
-// 스케줄 아이디는 모든 컴포넌트에서 공통적으로 사용해야하니까
-// useParams가 아닌 전역 상태관리를 해주자!
 
 const AllSemesterNamesForm = () => {
   const navigate = useNavigate();
@@ -44,6 +21,8 @@ const AllSemesterNamesForm = () => {
   const [selectedSemester, setSelectedSemester] = useState<string>(''); // 유저가 클릭한 학기
   const [isDropdownSemester, setIsDropdownSemester] = useState<boolean>(false); // 드롭다운
   const [defaultSemester, setDefaultSemester] = useState<string>('');
+
+  const currentUrl = useLocation();
   // 드롭다운 제어 함수
   const openDropdownSemester = () => {
     setIsDropdownSemester(true);
@@ -71,11 +50,23 @@ const AllSemesterNamesForm = () => {
   const handleClickSemester = (semesterName: string, semesterId: string) => {
     const selectedSemesterId = semesterOptions.find((s) => s.id === semesterId);
     // 선택한 스케줄 id url에 담기
+    console.log(4, currentUrl.pathname);
+
     if (selectedSemesterId) {
-      navigate(`/home/${selectedSemesterId.id}`);
+      // 현재 페이지에 따라서, navigate 달라짐
+      if (
+        currentUrl.pathname === '/home' ||
+        currentUrl.pathname === `/home/${scheduleId}`
+      ) {
+        navigate(`/home/${selectedSemesterId.id}`);
+      } else if (
+        currentUrl.pathname === '/timetable' ||
+        currentUrl.pathname === `/timetable/${scheduleId}`
+      ) {
+        navigate(`/timetable/${selectedSemesterId.id}`);
+      }
     }
-    // setSchedule({ id: selectedSemesterId.id }); // 전역에 스케줄아이디 저장
-    // localStorage.setItem('selectedSemester', semester); // 선택된 학기값을 로컬 스토리지에 저장
+
     setSelectedSemester(semesterName);
     closeDropdownSemester();
   };
