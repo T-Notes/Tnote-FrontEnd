a76f292cf1b7801343085e63bb1ba670ea3507f2
djwjfk <grw07236@naver.com>
2024-03-14 16:31:27 +0900

    fix: 서버 주소 수정

--- src/utils/InstanceAxios.tsx
+++ src/utils/InstanceAxios.tsx
@@ -7,7 +7,7 @@ interface JwtPayload {
 }
 
 const instanceAxios: AxiosInstance = axios.create({
-  baseURL: 'http://j9972.kr', //기본 url설정
+  baseURL: 'https://j9972.kr', //기본 url설정
   timeout: 5000, // 무한대기 현상 방지와 리소스 낭비를 줄이기 위해 설정
   headers: {
     'Content-Type': 'application/json',
