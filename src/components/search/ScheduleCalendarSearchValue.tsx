6f93cdc8f0d51f10d79f713e05e6f44c43d5ca6d
djwjfk <grw07236@naver.com>
2024-03-20 12:47:12 +0900

    Design: 일지 검색 ui

--- src/components/search/ScheduleCalendarSearchValue.tsx
+++ src/components/search/ScheduleCalendarSearchValue.tsx
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+
+const SSearchValueWrapper = styled.div`
+  display: flex;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  margin-left: 30px;
+  margin-right: 30px;
+  border-bottom: 1px solid #e8e8e8;
+  font-size: 16px;
+  font-weight: 500;
+  > div {
+    padding-right: 70px;
+  }
+`;
+
 interface SearchValue {
   id: number;
   studentName: string;
@@ -14,20 +30,20 @@ const ScheduleCalendarSearchValue = ({ searchValueList }: Props) => {
   return (
     <>
       {searchValueList.map((item, index) => {
-        console.log(item.startDate);
-
         const searchStartDate = item.startDate.slice(0, 10);
         const searchEndDate = item.endDate.slice(0, 10);
         const searchStartTime = item.startDate.slice(11, 16);
         const searchEndTime = item.endDate.slice(11, 16);
         return (
-          <div key={index}>
-            <div>{searchStartDate}</div>
-            <div>{searchEndDate}</div>
-            <div>{searchStartTime}</div>
-            <div>{searchEndTime}</div>
+          <SSearchValueWrapper key={index}>
+            <div>
+              {searchStartDate}~{searchEndDate}
+            </div>
+            <div>
+              {searchStartTime}~{searchEndTime}
+            </div>
             <div>{item.studentName || item.title}</div>
-          </div>
+          </SSearchValueWrapper>
         );
       })}
     </>
