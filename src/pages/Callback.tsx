9494f30307db5d02eb59652e7a98ff5dab7f29b1
djwjfk <grw07236@naver.com>
2024-04-17 02:16:17 +0900

    fix: 회원탈퇴 시 인가코드 전달하기

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -5,13 +5,10 @@ import instanceAxios from '../utils/InstanceAxios';
 
 const Callback = () => {
   const navigate = useNavigate();
-  const code = new URL(window.location.href).searchParams.get('code'); // 인가코드 받아보기
-
-  // 서버에 인가코드 넘겨주고, 토큰 값 받아오기
-  // 기존회원과 새회원 구분하여 페이지 이동하기
-  // 로그인을 해서 받아오는 정보에 회원정보 폼까지 작성한 유저인지, 아닌지 구분이 필요!
-  // 내 생각: 추가 회원정보 폼까지 다 작성한 유저라면, /home/id
-  // 그 이외에는 전부 /profileInfo
+  const code = new URL(window.location.href).searchParams.get('code');
+  if (code !== null) {
+    localStorage.setItem('code', code);
+  }
   const getToken = async () => {
     try {
       await instanceAxios
@@ -52,7 +49,6 @@ const Callback = () => {
 
   useEffect(() => {
     getToken();
-    const accessToken = localStorage.getItem('accessToken');
   }, [code]);
 
   return (
