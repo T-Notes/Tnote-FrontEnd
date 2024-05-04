ffab0e78e18569ee7123107b921b39134db53102
djwjfk <grw07236@naver.com>
2024-05-05 02:42:12 +0900

    fix: 랜딩 페이지 배경 화면 수정

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,28 +9,41 @@ import KakaoLoginBtn from '../components/Landing/KakaoLoginBtn';
 import PrivacyPolicyCheckbox from '../components/Landing/PrivacyPolicyCheckbox';
 import PrivacyPolicyModal from '../components/Landing/PrivacyPolicyModal';
 import { IcBackgroundIconGroup } from '../assets/icons';
-
+import { useModals } from '../utils/useHooks/useModals';
 // import LandingBackgroundImage from '../assets/images/LandingBackgroundImage.png';
 
 // styled //
 const SLandingWrapper = styled.div`
   // 개선 : 웹 화면 크기에 따라 달라져야 함
   /* background-image: url(${'../assets/images/LandingBackgroundImage.png'}); // 추후 수정하기 */
-  background-size: cover;
-  background-position: center;
+  background-color: #f5f6ff;
+  display: flex;
+  align-items: center;
+  /* align-items: center;
+  justify-content: center; */
+  /* background-size: cover;
+  background-position: center; */
   width: 100%;
   height: 100vh;
-  position: absolute;
+  /* position: absolute; */
 
   @media (max-width: 768px) {
     background-size: auto;
   }
 `;
+
+const SIcBackgroundIconGroup = styled(IcBackgroundIconGroup)`
+  padding-left: 60px;
+`;
+
+const SBackContent = styled.div`
+  display: flex;
+  margin-left: 18.75%;
+`;
 const SUnderbar = styled.div`
   margin-top: 32px;
   margin-bottom: 32px;
   width: 290px;
-  margin-left: 230px;
   border-bottom: 1px solid #d5d5d5;
 `;
 const SPrivacyPolicyText = styled.span`
@@ -43,60 +56,43 @@ const SPrivacyPolicyPointText = styled(SPrivacyPolicyText)`
 `;
 const SPrivacyPolicyCheckboxSection = styled.div`
   display: flex;
-  align-items: center; /* 세로축 가운데 정렬 */
-  margin-left: 230px;
+  align-items: center;
 `;
 
 const Landing = () => {
-  const [isWarning, setIsWarning] = useState<boolean>(false);
-  const [showWarningModal, setShowWarningModal] = useState<boolean>(false);
-
-  const { isOpen, openModal, closeModal } = useModal(); //개인정보 정책 모달
-  const { isToggle, setIsToggle, handleChangeToggle } = useToggle(); // checked
+  const { isToggle, setIsToggle, handleChangeToggle } = useToggle();
+  const { openModal, closeModal } = useModals();
 
   const handleIsCheckedTrue = () => {
-    closeModal();
+    closeModal(PrivacyPolicyModal);
     setIsToggle(true);
   };
 
-  const closeWarmingModal = () => {
-    setShowWarningModal(false);
+  const handleClickLandingModal = () => {
+    openModal(PrivacyPolicyModal, { closeModal, handleIsCheckedTrue });
   };
-  useEffect(() => {
-    if (isWarning) {
-      setShowWarningModal(true);
-
-      setIsWarning(false);
-    }
-  }, [isWarning]);
   return (
     <SLandingWrapper>
-      <IcBackgroundIconGroup />
-      <LandingLayout />
-      <KakaoLoginBtn onWarning={setIsWarning} isChecked={isToggle} />
-      <SUnderbar />
+      <SBackContent>
+        <div>
+          <LandingLayout />
+          <KakaoLoginBtn isChecked={isToggle} />
+          <SUnderbar />
 
-      <SPrivacyPolicyCheckboxSection>
-        <PrivacyPolicyCheckbox
-          isChecked={isToggle}
-          onCheckboxChange={handleChangeToggle}
-        />
-        <SPrivacyPolicyPointText onClick={openModal}>
-          개인 정보 보호 정책
-        </SPrivacyPolicyPointText>
-        <SPrivacyPolicyText>을 읽고 동의합니다.</SPrivacyPolicyText>
-      </SPrivacyPolicyCheckboxSection>
-      {/* 로그인 로직과 관련 */}
-      {showWarningModal && <WarningModal onClose={closeWarmingModal} />}
-      {isOpen && (
-        <>
-          <PrivacyPolicyModal
-            isOpen={isOpen}
-            onRequestClose={closeModal}
-            onPrivacyAgreement={handleIsCheckedTrue}
-          />
-        </>
-      )}
+          <SPrivacyPolicyCheckboxSection>
+            <PrivacyPolicyCheckbox
+              isChecked={isToggle}
+              onCheckboxChange={handleChangeToggle}
+            />
+            <SPrivacyPolicyPointText onClick={handleClickLandingModal}>
+              개인 정보 보호 정책
+            </SPrivacyPolicyPointText>
+            <SPrivacyPolicyText>을 읽고 동의합니다.</SPrivacyPolicyText>
+          </SPrivacyPolicyCheckboxSection>
+        </div>
+
+        <SIcBackgroundIconGroup />
+      </SBackContent>
     </SLandingWrapper>
   );
 };
