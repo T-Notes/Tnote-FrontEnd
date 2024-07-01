dc52748c4016c5c13397cc40a7493085d4549b9b
djwjfk <grw07236@naver.com>
2024-07-01 22:00:50 +0900

    Design: 네비게이션바 반응형 컴포넌트 제작

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,11 +1,27 @@
 import HomeNavigationBar from './common/HomeNavigationBar';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
+const SLayoutWrapper = styled.div`
+  position: fixed;
+`;
+
+const SContentWrapper = styled.div`
+  margin-left: 13.54vw;
+  @media (max-width: 960px) {
+    margin-left: 0px;
+  }
+`;
 const HomeLayout = () => {
   return (
     <>
-      <HomeNavigationBar />
-      <Outlet />
+      <SLayoutWrapper>
+        <HomeNavigationBar />
+      </SLayoutWrapper>
+
+      <SContentWrapper>
+        <Outlet />
+      </SContentWrapper>
     </>
   );
 };
