6637eacca63f9d9eb3e10fe37016f225cb1dcd34
djwjfk <grw07236@naver.com>
2024-07-30 16:17:36 +0900

    fix: 학교 검색 api code명 변경

--- src/components/ProfileInfo/RegionDropdownList.tsx
+++ src/components/ProfileInfo/RegionDropdownList.tsx
@@ -38,21 +38,21 @@ interface SchoolCityListProps {
 
 const RegionDropdownList = ({ onSelectedRegion }: SchoolCityListProps) => {
   const regionList = [
-    { cityNum: '100260', cityName: '서울특별시' },
-    { cityNum: '100267', cityName: '부산광역시' },
-    { cityNum: '100269', cityName: '인천광역시' },
-    { cityNum: '100272', cityName: '대구광역시' },
-    { cityNum: '100275', cityName: '광주광역시' },
-    { cityNum: '100271', cityName: '대전광역시' },
-    { cityNum: '100273', cityName: '울산광역시' },
-    { cityNum: '100276', cityName: '경기도' },
-    { cityNum: '100278', cityName: '강원도' },
-    { cityNum: '100281', cityName: '충청남도' },
-    { cityNum: '100280', cityName: '충청북도' },
-    { cityNum: '100285', cityName: '경상북도' },
-    { cityNum: '100291', cityName: '경상남도' },
-    { cityNum: '100282', cityName: '전라북도' },
-    { cityNum: '100292', cityName: '제주도' },
+    { code: 'B10', cityName: '서울특별시' },
+    { code: 'C10', cityName: '부산광역시' },
+    { code: 'E10', cityName: '인천광역시' },
+    { code: 'D10', cityName: '대구광역시' },
+    { code: 'F10', cityName: '광주광역시' },
+    { code: 'G10', cityName: '대전광역시' },
+    { code: 'H10', cityName: '울산광역시' },
+    { code: 'J10', cityName: '경기도' },
+    { code: 'K10', cityName: '강원도' },
+    { code: 'N10', cityName: '충청남도' },
+    { code: 'M10', cityName: '충청북도' },
+    { code: 'R10', cityName: '경상북도' },
+    { code: 'S10', cityName: '경상남도' },
+    { code: 'P10', cityName: '전라북도' },
+    { code: 'T10', cityName: '제주도' },
   ];
   return (
     <>
@@ -60,7 +60,7 @@ const RegionDropdownList = ({ onSelectedRegion }: SchoolCityListProps) => {
         <SItemContainer>
           {regionList.map((region) => (
             <SList
-              key={region.cityNum}
+              key={region.code}
               onClick={() => onSelectedRegion(region.cityName)}
             >
               <SItem>{region.cityName}</SItem>
